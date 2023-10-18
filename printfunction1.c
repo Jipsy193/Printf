ab638b9bfa99fb38612c61c6e119671642fc0ca4
RachaelAjayi <yemak31@gmail.com>
2023-10-17 21:36:43 -0700

    printf

--- printfunction1.c
+++ printfunction1.c
@@ -12,12 +12,12 @@
 int print_unsigned(va_list arg_types, char buffer[],
 int flags, int width, int precision, int size)
 {
-int i = BUFF_SIZE - 2;
+int i = BUFFER_SIZE - 2;
 unsigned long int num = va_arg(arg_types, unsigned long int);
 num = convert_size_unsgnd(num, size);
 if (num == 0)
 buffer[i--] = '0';
-buffer[BUFF_SIZE - 1] = '\0';
+buffer[BUFFER_SIZE - 1] = '\0';
 while (num > 0)
 {
 buffer[i--] = (num % 10) + '0';
@@ -39,14 +39,14 @@ return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 int print_octal(va_list arg_types, char buffer[],
 int flags, int width, int precision, int size)
 {
-int i = BUFF_SIZE - 2;
+int i = BUFFER_SIZE - 2;
 unsigned long int num = va_arg(arg_types, unsigned long int);
 unsigned long int init_num = num;
 UNUSED(width);
 num = convert_size_unsgnd(num, size);
 if (num == 0)
 buffer[i--] = '0';
-buffer[BUFF_SIZE - 1] = '\0';
+buffer[BUFFER_SIZE - 1] = '\0';
 while (num > 0)
 {
 buffer[i--] = (num % 8) + '0';
@@ -106,7 +106,7 @@ flags, 'X', width, precision, size));
 int print_hexa(va_list arg_types, char map_to[], char buffer[],
 int flags, char flag_ch, int width, int precision, int size)
 {
-int i = BUFF_SIZE - 2;
+int i = BUFFER_SIZE - 2;
 
 unsigned long int num = va_arg(arg_types, unsigned long int);
 unsigned long int init_num = num;
@@ -114,7 +114,7 @@ UNUSED(width);
 num = convert_size_unsgnd(num, size);
 if (num == 0)
 buffer[i--] = '0';
-buffer[BUFF_SIZE - 1] = '\0';
+buffer[BUFFER_SIZE - 1] = '\0';
 while (num > 0)
 {
 buffer[i--] = map_to[num % 16];
@@ -128,4 +128,3 @@ buffer[i--] = '0';
 i++;
 return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
-
